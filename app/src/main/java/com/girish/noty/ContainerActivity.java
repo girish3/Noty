cdbcc4118b49543d4bffc2c0464c044cebfd0bd6
girish <girish.budhwani@freecharge.com>
2016-06-19 19:46:13 +0530

    noty project created

---
+++ app/src/main/java/com/girish/noty/ContainerActivity.java
@@ -0,0 +1,13 @@
+package com.girish.noty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
